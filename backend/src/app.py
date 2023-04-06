08cce29053eec1b7fa7179eca5ac0f17dbadbe4e
Emanuele Fratocchi <emanuele.fratochi@noovle.com>
2023-04-06 10:32:06 +0200

    added backend folder

---
+++ backend/src/app.py
@@ -0,0 +1,38 @@
+from flask import Flask, jsonify, request
+from models.train_model import MNISTModel
+from models.predict_model import get_prediction
+import torch
+import os 
+
+root = os.getcwd()
+
+app = Flask(__name__)
+
+#define model 
+model_path= "lightning_logs/version_0/checkpoints/epoch=2-step=2814.ckpt"
+model = MNISTModel.load_from_checkpoint(model_path)
+model.eval()
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        # we will get the file from the request
+        try:
+            file = request.files['file']
+        except KeyError:
+            return jsonify({'error': 'no text sent'})
+
+        img_bytes = file.read()
+        y_hat = get_prediction(model, image_bytes=img_bytes)
+        return {
+            'output':str(torch.argmax(y_hat).item())
+        }
+        
+@app.route('/', methods=['GET'])
+def home():
+    return 'Welcome to digit recognizer app!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True) 
+
+  
